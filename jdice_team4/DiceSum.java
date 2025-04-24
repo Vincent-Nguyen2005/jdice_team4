aa6cff1997c9f5c66ddddd8af08b2e975c4ce7e8
hyxiu147 <154442640+hyxiu147@users.noreply.github.com>
2025-04-24 15:01:13 +0700

    Sửa lỗi DiceSum
    Sửa lỗi khai báo biến: thêm dấu cách giữa từ khóa và kiểu dữ liệu.
    Sửa tên constructor từ Dice_Sum thành DiceSum cho đúng quy tắc Java.

--- jdice_team4/DiceSum.java
+++ jdice_team4/DiceSum.java
@@ -20,19 +20,22 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 public class DiceSum extends DieRoll {
-    privateDieRoll r1;
-//    private DieRoll r2;
-    public Dice_Sum(DieRoll r1,
-		   DieRoll r2) {
-	super(0,0,0)
-	this.r1=r1;
-	thisr2=r2;
-    }
-    public RollResult makeRoll() {
-	return r1.makeRoll().andThen(r2.makeRoll());
-    }
-    public String toString() {
-	return r1.toString() + " & "+r2.toString();
+    private DieRoll r1;
+    private DieRoll r2;
+
+    public DiceSum(DieRoll r1, DieRoll r2) {
+        super(0, 0, 0); // Gọi constructor cha
+        this.r1 = r1;
+        this.r2 = r2;
     }
 
+    @Override
+    public RollResult makeRoll() {
+        return r1.makeRoll().andThen(r2.makeRoll());
+    }
+
+    @Override
+    public String toString() {
+        return r1.toString() + " & " + r2.toString();
+    }
 }
